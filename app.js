074e54a669c21c2075e09c158507d3d3fb3b767e
Arihant <dugarrarihantt2015@gmail.com>
2022-01-12 21:55:47 +0530

    Initial

--- app.js
+++ app.js
@@ -115,7 +115,8 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/dsxxey5fn/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/douqbebwk/",
                 "https://images.unsplash.com",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
